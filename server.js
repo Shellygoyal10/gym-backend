7e956c624ef8997998a11144d8ad015bc51a45e8
enthusiasticreader <shellygoyal836@gmail.com>
2026-04-09 14:56:36 +0530

    Update server.js
    port fix for deployment

--- server.js
+++ server.js
@@ -105,6 +105,8 @@ app.get('/trainers', (req, res) => {
 
 
 // ── START SERVER ─────────────────────────────────────────────────────────────
-app.listen(3000, () => {
-  console.log('Server running on port 3000 🚀');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
